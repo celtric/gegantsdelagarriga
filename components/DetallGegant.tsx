71f8ef137f43eefce98da356915e242708744784
Ricard Osorio <celtric@gmail.com>
2022-11-07 17:30:28 +0100

    Add gegants' images

--- components/DetallGegant.tsx
+++ components/DetallGegant.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import {Box, Grid, Typography} from "@mui/material";
 import Image from "next/image";
+import {StaticImageData} from "next/dist/client/image";
 
 type Props = {
   name: string,
   height: string,
   weight: string,
-  photo: string,
+  image: StaticImageData,
   petit?: boolean,
   children?: React.ReactNode
 };
 
-const DetallGegant: React.FC<Props> = ({name, height, weight, photo, petit, children}) => {
+const DetallGegant: React.FC<Props> = ({name, height, weight, image, petit, children}) => {
   return <Box sx={{p: 4, pb: 2, mb: 3, bgcolor: "#f6f8fa"}}>
     <Grid container spacing={3}>
       <Grid item>
-        <Image src="/gegants-de-la-garriga.jpg" alt={`Fotografia de ${name}`} width={200} height={200} />
-        {/*<img src={photo} alt={`Fotografia de ${name}`} width={200} height={200} />*/}
+        <Image src={image} alt={`Fotografia de ${name}`} width={200} height={200} />
       </Grid>
       <Grid item xs>
         <Typography component="h2" variant="h2">{name}</Typography>
