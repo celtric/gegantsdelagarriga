312478e444f936dd286be7ab3c23aafa3a9edf0a
Ricard Osorio <celtric@gmail.com>
2022-11-15 16:37:42 +0100

    Calendar

--- pages/calendari/index.tsx
+++ pages/calendari/index.tsx
@@ -1,61 +1,40 @@
 import React from "react";
 import Head from "next/head";
-import {Button, Card, CardActions, CardContent, CardMedia, Container, Divider, Grid, Typography} from "@mui/material";
+import {Card, CardContent, CardMedia, Container, Typography} from "@mui/material";
+import FotoCursaSantSilvestre from "../../public/cursa-sant-silvestre.jpg";
 
 const Index: React.FC = () => {
   return <>
     <Head><title>Calendari</title></Head>
-    <Container maxWidth="md" sx={{mt: 5, mb: 10}} disableGutters>
+    <Container maxWidth="sm" sx={{mt: 5, mb: 10}} disableGutters>
       <Typography component="h1" variant="h1" gutterBottom>Calendari</Typography>
-      <Grid container direction="column" spacing={5} alignItems="center">
-        <Grid item>
-          <Card>
-            <CardMedia
-              component="img"
-              height="140"
-              image="https://i.imgur.com/oT1j3o1.png"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Sant Esteve de Palautordera &middot; 30 Octubre 2022
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pharetra bibendum massa sed lobortis.
-                Donec id leo non odio imperdiet commodo id vel mauris. Aliquam vulputate scelerisque dui, sit amet
-                ultrices lacus vestibulum eu. Nam consectetur arcu eget venenatis egestas. Aliquam at vehicula augue.
-                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vitae lacus
-                ipsum.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Pàgina oficial de la trobada</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item container spacing={5}>
-          {[0, 1].map(n => <Grid key={n} item>
-            <Card sx={{maxWidth: 400}}>
-              <CardMedia
-                component="img"
-                height="140"
-                image="https://i.imgur.com/oT1j3o1.png"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                  Sant Esteve de Palautordera &middot; 30 Octubre 2022
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pharetra bibendum massa sed lobortis.
-                  Donec id leo non odio imperdiet commodo id vel mauris.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Pàgina oficial de la trobada</Button>
-              </CardActions>
-            </Card>
-          </Grid>)}
-        </Grid>
-      </Grid>
+      <Card sx={{mb: 5}}>
+        <CardMedia component="img" height="140" image={FotoCursaSantSilvestre.src} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Propera sortida: Cursa de Sant Silvestre de la Garriga
+          </Typography>
+          <Typography gutterBottom component="div">
+            31 Desembre 2022
+          </Typography>
+          <Typography paragraph color="text.secondary">
+            Com cada any, participarem en la tradicional cursa de Sant Silvestre que organitzar el Club atletisme les
+            Tortugues de la Garriga. Ens veiem allà!
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Fi de la temporada 😢
+          </Typography>
+          <Typography paragraph color="text.secondary">
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            La cursa de Sant Silvestre serà l'última sortida dels nostres gegants preferits. Però d'aquí poc penjarem el
+            calendari de 2023!
+          </Typography>
+        </CardContent>
+      </Card>
     </Container>
   </>;
 };
