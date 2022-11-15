e5d29d47f9b3c2c7b208f9a22f4cdfa943880799
Ricard Osorio <celtric@gmail.com>
2022-11-15 16:58:20 +0100

    Improve calendar

--- pages/calendari/index.tsx
+++ pages/calendari/index.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import Head from "next/head";
 import {Card, CardContent, CardMedia, Container, Typography} from "@mui/material";
 import FotoCursaSantSilvestre from "../../public/cursa-sant-silvestre.jpg";
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator
+} from "@mui/lab";
+import {CalendarMonth, DirectionsRun, Hotel, SportsScore} from "@mui/icons-material";
 
 const Index: React.FC = () => {
   return <>
@@ -23,18 +33,110 @@ const Index: React.FC = () => {
           </Typography>
         </CardContent>
       </Card>
-      <Card>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Fi de la temporada 😢
-          </Typography>
-          <Typography paragraph color="text.secondary">
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
-            La cursa de Sant Silvestre serà l'última sortida dels nostres gegants preferits. Però d'aquí poc penjarem el
-            calendari de 2023!
-          </Typography>
-        </CardContent>
-      </Card>
+      <Timeline position="right">
+        <TimelineItem>
+          <TimelineOppositeContent sx={{m: "auto 0"}} color="text.secondary">
+            Gener 2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="success"><SportsScore /></TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{py: 2}}>
+            <Typography variant="h6" component="span">
+              Inici de temporada 2022
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot>
+              <CalendarMonth />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{py: 2}}>
+            <Typography variant="h6" component="span">
+              Sortides vàries
+            </Typography>
+            <Typography>Cercaviles a altres pobles, activitats de festa major...</Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent sx={{m: "auto 0"}} color="text.secondary">
+            Agost 2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot>
+              <Hotel />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{py: 2}}>
+            <Typography variant="h6" component="span">
+              Vacances
+            </Typography>
+            <Typography>Un mes per carregar les piles!</Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot>
+              <CalendarMonth />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{py: 2}}>
+            <Typography variant="h6" component="span">
+              Sortides vàries
+            </Typography>
+            <Typography>Cercaviles a altres pobles</Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent sx={{m: "auto 0"}} color="text.secondary">
+            31 Desembre 2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="primary">
+              <DirectionsRun />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{py: 2}}>
+            <Typography variant="h6" component="span">
+              Cursa de Sant Silvestre
+            </Typography>
+            <Typography>
+              Participarem en la tradicional cursa de Sant Silvestre que organitza el Club atletisme les Tortugues de la Garriga
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent sx={{m: "auto 0"}} color="text.secondary">
+            31 Desembre 2022
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="error"><SportsScore /></TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{py: 2}}>
+            <Typography variant="h6" component="span">
+              Fi de temporada 2022 😢
+            </Typography>
+            <Typography>
+              La cursa de Sant Silvestre serà l&apos;última sortida dels nostres gegants preferits. Però d&apos;aquí poc
+              penjarem el calendari de 2023!
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+      </Timeline>
     </Container>
   </>;
 };
