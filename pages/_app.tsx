dba8882613015f6da70db661cea282a747e8678e
Ricard Osorio <celtric@gmail.com>
2023-02-27 10:09:18 +0100

    Add Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Footer from "./_footer";
 import {Box, Container, CssBaseline} from "@mui/material";
 import {AppProps} from "next/app";
 import {theme} from "../theme";
+import {Analytics} from "@vercel/analytics/react";
 
 const App: React.FC<AppProps> = ({Component, pageProps}) => <ThemeProvider theme={theme}>
   <CssBaseline />
@@ -12,6 +13,7 @@ const App: React.FC<AppProps> = ({Component, pageProps}) => <ThemeProvider theme
   <Box component="main" sx={{bgcolor: "#fff", p: 1, "& img": {maxWidth: "100%"}}}>
     <Container maxWidth="lg" sx={{mt: {xs: 2, md: 5}, mb: {xs: 5, md: 10}}}>
       <Component {...pageProps} />
+      <Analytics />
     </Container>
   </Box>
   <Footer />
