73a43f18dfd820a7191ed737d32e52a46e44be28
Ricard Osorio <celtric@gmail.com>
2023-03-14 07:46:26 +0100

    Prepare Racó geganer

---
+++ components/RacoGeganter.tsx
@@ -0,0 +1,82 @@
+import React, {useState} from "react";
+import Head from "next/head";
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Paper,
+  Typography
+} from "@mui/material";
+import {ArrowRight, Close, KeyboardArrowRight, Menu} from "@mui/icons-material";
+import NextLink from "next/link";
+import {MDXProvider} from "@mdx-js/react";
+import {racoGeganterPages} from "../data";
+
+const mdxComponents = {
+  h1: ({children}: any) => <Typography component="h1" variant="h1" sx={{mb: 4}}>{children}</Typography>,
+  h2: ({children}: any) => <Typography component="h2" variant="h2" gutterBottom>{children}</Typography>,
+  p: ({children}: any) => <Typography paragraph>{children}</Typography>,
+};
+
+type Props = {
+  title: string,
+  selectedRoute: string,
+  children: React.ReactNode
+};
+
+const RacoGeganter: React.FC<Props> = ({title, selectedRoute, children}) => {
+  const [isMobileMenuVisible, toggleMobileMenu] = useState(false);
+  return <MDXProvider components={mdxComponents}>
+    <Head><title>{title}</title></Head>
+    <Container maxWidth="lg" disableGutters>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={0} lg={1} sx={{display: {xs: "none", lg: "inherit"}}} />
+        <Grid item xs={12} md={4} lg={3} sx={{borderRight: {md: "8px solid #f6f8fa"}}}>
+          <Paper sx={{display: {xs: "block", md: "none"}, textAlign: "center"}}>
+            <MenuList disablePadding>
+              <MenuItem onClick={() => toggleMobileMenu(!isMobileMenuVisible)}>
+                <ListItemIcon sx={{textAlign: "center"}}>
+                  {!isMobileMenuVisible && <Menu />}
+                  {isMobileMenuVisible && <Close />}
+                </ListItemIcon>
+                <ListItemText primaryTypographyProps={{fontWeight: 700, marginLeft: -5}}>
+                  {title}
+                </ListItemText>
+              </MenuItem>
+            </MenuList>
+          </Paper>
+          <List sx={{display: {xs: isMobileMenuVisible ? "block" : "none", md: "block"}}}>
+            {racoGeganterPages.map((page, index) => <React.Fragment key={index}>
+              {page.route && page.route !== selectedRoute && <ListItemButton
+                component={NextLink}
+                href={page.route}
+                sx={{"&:hover": {backgroundColor: "#f6f8fa", textDecoration: "underline"}}}>
+                {page.indented && <ListItemIcon sx={{ml: 1, mr: -3}}><KeyboardArrowRight /></ListItemIcon>}
+                <ListItemText>{page.name}</ListItemText>
+              </ListItemButton>}
+              {page.route && page.route === selectedRoute && <ListItem sx={{backgroundColor: "#f6f8fa"}}>
+                {page.indented && <ListItemIcon sx={{ml: 1, mr: -3}}><KeyboardArrowRight /></ListItemIcon>}
+                <ListItemText sx={{"*": {fontWeight: "bold !important"}}}>{page.name}</ListItemText>
+                <ListItemIcon sx={{mr: -5, color: "#000", display: {xs: "none", md: "block"}}}><ArrowRight /></ListItemIcon>
+              </ListItem>}
+              {!page.route && <ListItem>
+                <ListItemText>{page.name}:</ListItemText>
+              </ListItem>}
+            </React.Fragment>)}
+          </List>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          {children}
+        </Grid>
+      </Grid>
+    </Container>
+  </MDXProvider>;
+};
+
+export default RacoGeganter;
--- data.ts
+++ data.ts
@@ -2,40 +2,101 @@ import FotoXixilonaIBenet from "public/xixilona-i-benet.jpg";
 import FotoCalendari from "public/calendari.png";
 import FotoHistoria from "public/historia.jpg";
 import FotoLaColla from "public/la-colla-home.jpg";
+import {StaticImageData} from "next/dist/client/image";
 
-export const pages = [
+export const pages: { title: string, route: string, isSelected: (route: string) => boolean, description?: string, image?: StaticImageData }[] = [
   {
     title: "Inici",
-    route: "/"
+    route: "/",
+    isSelected: (route: string) => route === "/"
   },
   {
     title: "Els gegants",
     route: "/els-gegants",
+    isSelected: (route: string) => route === "/els-gegants",
     description: "Coneix a la Xixilona, en Magí, en Benet, en Garric, en Verdet i en Mussolet!",
     image: FotoXixilonaIBenet
   },
   {
     title: "Calendari",
     route: "/calendari",
+    isSelected: (route: string) => route === "/calendari",
     description: "Informació de les properes sortides que faran els gegants durant l'any.",
     image: FotoCalendari
   },
   {
     title: "Història",
     route: "/historia",
+    isSelected: (route: string) => route === "/historia",
     description: "Els nostres gegants han viscut una vida interessant durant més de 80 anys, coneix-la!",
     image: FotoHistoria
   },
   {
-    title: "La colla",
+    title: "Racó geganter",
     route: "/la-colla",
+    isSelected: (route: string) => racoGeganterPages.map(page => page.route as string).includes(route),
     description: "Curiositats de la colla, materials pels més petits...",
     image: FotoLaColla
   },
   {
     title: "Contacte",
-    route: "/contacte"
+    route: "/contacte",
+    isSelected: (route: string) => route === "/contacte"
   }
+];
+
+export const racoGeganterPages: {
+  name: string,
+  route?: "/la-colla" | "/la-musica" | "/els-assajos" | "/pintables-de-gegants" | "/gegants-de-cartro" | "/figures-3d" | "/robots-geganters" | "/els-vestits" | "/les-disfresses" | "/records-de-trobades",
+  indented?: boolean
+}[] = [
+  {
+    name: "La colla",
+    route: "/la-colla"
+  },
+  {
+    name: "La música",
+    route: "/la-musica"
+  },
+  {
+    name: "Els assajos",
+    route: "/els-assajos"
+  },
+  {
+    name: "Pels més petits"
+  },
+  {
+    name: "Pintables de gegants",
+    route: "/pintables-de-gegants",
+    indented: true
+  },
+  {
+    name: "Gegants de cartró",
+    route: "/gegants-de-cartro",
+    indented: true
+  },
+  {
+    name: "Figures 3D",
+    route: "/figures-3d",
+    indented: true
+  },
+  {
+    name: "Robots geganters",
+    route: "/robots-geganters",
+    indented: true
+  },
+  {
+    name: "Els vestits",
+    route: "/els-vestits"
+  },
+  {
+    name: "Les disfresses",
+    route: "/les-disfresses"
+  },
+  {
+    name: "Records de les trobades",
+    route: "/records-de-trobades"
+  },
 ];
 
 export const instagramLink = "https://www.instagram.com/gegantsdelagarriga/";
