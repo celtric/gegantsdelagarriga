3832f73b74d22419894115d334272272a5470994
Ricard Osorio <celtric@gmail.com>
2023-03-18 07:28:22 +0100

    Update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ import {Head, Html, Main, NextScript} from "next/document";
 // noinspection HtmlRequiredTitleElement
 const Document: React.FC = () => <Html>
   <Head>
+    <link rel="icon" href="/favicon.png" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet" />
   </Head>
   <body>
